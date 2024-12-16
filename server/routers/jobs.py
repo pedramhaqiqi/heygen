98bcf2e0d3ccbdecb57eca6c4c37be507e1ab413
Pedram Haqiqi <63091219+pedramhaqiqi@users.noreply.github.com>
2024-12-15 21:41:16 -0500

    Create job creation endpoint

---
+++ server/routers/jobs.py
@@ -0,0 +1,20 @@
+# server/routers/jobs.py
+from fastapi import APIRouter, Request
+from pydantic import BaseModel
+from server.core.task import Task
+
+router = APIRouter()
+
+
+class CreateJobRequest(BaseModel):
+    processing_duration: float
+    should_error: bool
+
+
+@router.post("/jobs")
+def create_job(req: Request, body: CreateJobRequest):
+    task = Task(
+        processing_duration=body.processing_duration, should_error=body.should_error
+    )
+    req.app.state.task_registry.add_task(task)
+    return {"job_id": task.job_id, "status": task.get_status()}
