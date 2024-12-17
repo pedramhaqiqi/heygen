94ccba567f2100bce232ddb3706abd7a97d10095
Pedram Haqiqi <63091219+pedramhaqiqi@users.noreply.github.com>
2024-12-17 04:49:50 -0500

    Include necessary exports for public facing API

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,9 @@
-export { JobClient } from './JobClient';  
+export { JOB_STATUS, POLLING_MODES } from "./constants";
+export {
+  JobClientError,
+  NetworkError,
+  RateLimitError,
+  ServerError,
+} from "./errors";
+export { JobClient } from "./JobClient";
+export { AwaitCompletionOptions, JobStatus, PollingMode } from "./types";
