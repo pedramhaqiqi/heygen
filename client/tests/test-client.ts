ab5c7ab09a5c207bf3bf37becf4ea93c5e33f5cb
Pedram Haqiqi <63091219+pedramhaqiqi@users.noreply.github.com>
2024-12-17 04:16:49 -0500

    Add docs to simple test script

--- client/tests/test-client.ts
+++ client/tests/test-client.ts
@@ -1,6 +1,20 @@
+/**
+ * A simple script to demonstrate basic functionality of the JobClient.
+ *
+ * This script creates a new job using the JobClient, waits for its completion,
+ * and logs the job's final status.
+ *
+ * To run this script, ensure that the server is running locally on port 8000.
+ */
 import { JobClient } from "../src";
+import { AwaitCompletionOptions } from "../src/types";
 
 const BASE_URL: string = "http://localhost:8000";
+const pollingConfig: AwaitCompletionOptions = {
+  timeoutMs: 20000,
+  pollIntervalMs: 1000,
+  mode: "long",
+}
 
 async function runTest() {
   const client = new JobClient(BASE_URL);
@@ -12,14 +26,10 @@ async function runTest() {
     console.log(`Job created with ID: ${job_id}, initial status: ${status}`);
 
     console.log("Waiting for job completion...");
-    const finalStatus = await client.awaitCompletion(job_id, {
-      timeoutMs: 20000,
-      pollIntervalMs: 0,
-      mode: "long",
-    });
+    const finalStatus = await client.awaitCompletion(job_id, pollingConfig);
     console.log(`Final job status for job ${job_id}: ${finalStatus}`);
   } catch (error) {
-    console.error("Error while waiting for job completion:", error);
+    console.error(error);
   }
 }
 
